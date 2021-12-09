081feb00c3c6f923b21d06a419f500bf30cb3aa2
bukin <bukindima@mail.ru>
2021-12-10 01:09:28 +0400

    Refactoring Added slf4j

--- src/main/java/com/example/antispamlpbot/AntiSpamLPbot.java
+++ src/main/java/com/example/antispamlpbot/AntiSpamLPbot.java
@@ -1,26 +1,26 @@
 package com.example.antispamlpbot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.groupadministration.BanChatMember;
-import org.telegram.telegrambots.meta.api.methods.groupadministration.KickChatMember;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
-import org.telegram.telegrambots.meta.api.objects.*;
-import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.time.Instant;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Component
 public class AntiSpamLPbot extends TelegramLongPollingBot {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AntiSpamLPbot.class);
+    private static final Integer SUSPECT_PERIOD = 60;
+    private static final String ANTI_SPAM_LP_BOT = "AntiSpamLPbot";
 
     @Value("${AntiSpamLPbot.username}")
     private String username;
@@ -28,66 +28,81 @@ public class AntiSpamLPbot extends TelegramLongPollingBot {
     @Value("${AntiSpamLPbot.token}")
     private String token;
 
-    private List<SuspectUser> suspectUsers = new ArrayList<>();
+    private final List<SuspectUser> suspectUsers = new ArrayList<>();
 
+    private Update update;
 
     @Override
-    public void onUpdateReceived(Update update) {
-
-        //String userName = update.getMessage().getFrom().getUserName();
-        Integer messageTime = update.getMessage().getDate();
-        Integer suspectMessageTime = 0;
-        if (update.hasMessage()) {
-            long suspectUserId = update.getMessage().getFrom().getId();
-            for (SuspectUser suspectUser : suspectUsers) {
-                if (suspectUser.getId() == suspectUserId)
-                    suspectMessageTime = messageTime;
-            }
-
-            List<User> users = update.getMessage().getNewChatMembers();
-            System.out.println("Хуета от телеги" + users);
-            for (int i = 0; i < users.size(); ++i) {
-                SuspectUser temp = new SuspectUser(users.get(i).getId(), Instant.now().getEpochSecond());
-
-                suspectUsers.add(temp);
-                System.out.println("наша хуета:" + temp);
-            }
-            if (update.getMessage().hasText() && (update.getMessage().getDate() - suspectMessageTime) < 10) {
-                String chatId = update.getMessage().getChatId().toString();
-                Integer messageId = update.getMessage().getMessageId();
-                DeleteMessage deleteMessage = new DeleteMessage(chatId, messageId);
-                BanChatMember banChatMember = new BanChatMember(chatId, suspectUserId);
-                try {
-                    execute(deleteMessage);
-                    execute(banChatMember);
-                } catch (TelegramApiException e) {
-                    //todo add logging to the project.
-                    e.printStackTrace();
+    public void onUpdateReceived(Update newUpdate) {
+        LOGGER.debug("AYE");
+        update = newUpdate;
+        if (!update.hasMessage()) {
+            LOGGER.debug("No Message.");
+            return;
+        }
+        Boolean isGuiltyContent = isGuiltyContent();
+        LOGGER.debug("Is Guilty Content: {}", isGuiltyContent);
+        if (!isGuiltyContent) {
+            return;
+        }
+        long userId = update.getMessage().getFrom().getId();
+        for (SuspectUser suspectUser : suspectUsers) {
+            if (userId == suspectUser.getId()) {
+                if (isGuilty(suspectUser)) {
+                    banUser(userId);
+                    suspectUsers.remove(suspectUser);
+                    LOGGER.info("User With Username: {} - Successfully Banned.", suspectUser.getUserName());
+                    break;
                 }
             }
-
-            System.out.println(suspectUsers);
-            String chatId = update.getMessage().getChatId().toString();
-            SendMessage sm = new SendMessage();
-            Long userId = update.getMessage().getFrom().getId();
-
-
-            sm.setChatId(chatId);
-            sm.setText("bukinpidor");
-
-
         }
+        update.getMessage().getNewChatMembers().forEach(this::addNewUserToSuspectUsers);
+    }
+
+    private Boolean isGuiltyContent() {
+        //todo: guilty searcher logic
+        return true;
+    }
+
+    private void banUser(Long userId) {
+        Long chatId = update.getMessage().getChatId();
+        Integer messageId = update.getMessage().getMessageId();
+        LOGGER.info("Chat Id: {}, Message Id: {}", chatId, messageId);
+        try {
+            execute(new DeleteMessage(String.valueOf(chatId), messageId));
+            execute(new BanChatMember(String.valueOf(chatId), userId));
+        } catch (TelegramApiException exception) {
+            LOGGER.error("Error During Ban Operation: ", exception);
+        }
+    }
+
+    public void addNewUserToSuspectUsers(User user) {
+        if (ANTI_SPAM_LP_BOT.equals(user.getUserName())) {
+            return;
+        }
+        SuspectUser suspectUser = new SuspectUser(user.getId(), user.getUserName(), Instant.now().getEpochSecond());
+        LOGGER.info("New Suspected User: {}", suspectUser);
+        suspectUsers.add(suspectUser);
+    }
+
+    private Boolean isGuilty(SuspectUser suspectUser) {
+        boolean isHasText = update.getMessage().hasText();
+        Boolean isSuspectTimeEnded = (update.getMessage().getDate() - suspectUser.getJoinDate()) > SUSPECT_PERIOD;
+        if (isSuspectTimeEnded) {
+            LOGGER.info("Suspected User: {} - Confirmed", suspectUser);
+            suspectUsers.remove(suspectUser);
+        }
+        LOGGER.info("Is Has Text: {}, Is Suspect Time Ended: {}", isHasText, isSuspectTimeEnded);
+        return isHasText && !isSuspectTimeEnded;
     }
 
     @Override
     public String getBotUsername() {
-
         return username;
     }
 
     @Override
     public String getBotToken() {
-
         return token;
     }
 }
--- src/main/java/com/example/antispamlpbot/SuspectUser.java
+++ src/main/java/com/example/antispamlpbot/SuspectUser.java
@@ -1,18 +1,16 @@
 package com.example.antispamlpbot;
 
-
 import lombok.Data;
-import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import java.time.Instant;
-import java.util.Date;
+
 @Data
 public class SuspectUser {
-    private Long  id;
+    private Long id;
+    private String userName;
     private Long joinDate;
 
-    public SuspectUser(Long id, Long joinDate) {
+    public SuspectUser(Long id, String userName, Long joinDate) {
         this.id = id;
+        this.userName = userName;
         this.joinDate = joinDate;
     }
 }
